26bf38a2adfb4524922a9096c4972b33a1ff1a71
saavnkr <87351765+saavnkr@users.noreply.github.com>
2022-03-21 22:49:44 +0530

    practice code for reference
    basic practice codes, and learning how to use GITHUB desktop

---
+++ practice_data_science.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+sales = pd.read_csv(r'C:\Users\sawan kr\Desktop\python practice\music.csv')
+# print(sales)
+print(sales.head())
---
+++ python_numpy.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([1,3,4,5,7])
+y = np.array([4,7,8,9,10])
+
+plt.plot(x,y)
+plt.show()
