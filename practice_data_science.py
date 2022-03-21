db45abb09881e84353d3fce6fbba1acee31cd877
saavnkr <87351765+saavnkr@users.noreply.github.com>
2022-03-21 22:54:54 +0530

    Update practice_data_science.py

--- practice_data_science.py
+++ practice_data_science.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+#opening a csv file
 sales = pd.read_csv(r'C:\Users\sawan kr\Desktop\python practice\music.csv')
 # print(sales)
 print(sales.head())
